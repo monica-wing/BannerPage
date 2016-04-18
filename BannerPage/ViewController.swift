5bddbd5fb7cd3761a5e992ab05daa945ea175791
ir0ach <ir0achn@gmail.com>
2016-04-18 14:34:37 +0800

    Initial Commit

---
+++ BannerPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BannerPage
+//
+//  Created by 张霄男 on 16/4/18.
+//  Copyright © 2016年 ir0ach.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
