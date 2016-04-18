2df2be7a0f8f90a63d855db94395ff99421c7101
ir0ach <ir0achn@gmail.com>
2016-04-18 15:18:05 +0800

    ViewController添加BannerPage的代理

--- BannerPage/ViewController.swift
+++ BannerPage/ViewController.swift
@@ -14,14 +14,18 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     
     let banner = BannerPageView(frame: CGRect(x: 0, y: 0, width: view.width, height: view.width / 2))
+    banner.delegate = self
     view.addSubview(banner)
     banner.imagesURL = [
       "http://qsqseapp-10000035.image.myqcloud.com/qqtbossdevcom/banner/8c6c900824bc7c934e42361ca134ee27.png",
       "http://qsqseapp-10000035.image.myqcloud.com/qqtbossdevcom/banner/0e3fc408497be65a68ef137a517445f2.png"
       ]
   }
-  
-
 
 }
 
+extension ViewController: BannerPageViewDelegate {
+  func pageViewDidClick(pageView: BannerPageView, currentPage: Int) {
+    print("当前点击了第\(currentPage)页")
+  }
+}
